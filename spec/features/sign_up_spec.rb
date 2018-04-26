49bcdddbb34ecdd8581c4ac536c0ac064ed9a22f
funatsu <keisuekyan2012@gmail.com>
2018-04-26 13:57:27 +0900

    Test for user registration

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -18,6 +18,40 @@ RSpec.feature "Sign up", type: :feature do
     fill_in 'shop[email]', with: 'example@example.com'
     fill_in 'shop[password]', with: '12345678'
     fill_in 'shop[password_confirmation]', with: '12345678'
-    expect { click_button 'Sign up' }.to change { ActionMailer::Base.deliveries.size }.by(1)
+    expect { click_button '登録' }.to change { ActionMailer::Base.deliveries.size }.by(1)
+    expect(page).to have_content '確認メールを登録したメールアドレス宛に送信しました'
+
+    token = Shop.where(email:'example@example.com').first.confirmation_token
+    visit shop_confirmation_url(confirmation_token: token)
+    expect(page).to have_content 'アカウントが確認されました'
+
+    visit root_path
+    click_link 'ログイン'
+    fill_in 'shop[email]', with: 'example@example.com'
+    fill_in 'shop[password]', with: '12345678'
+    click_button 'ログイン'
+    expect(page).to have_content 'ログインしました'
+
+    click_link 'ログアウト'    
+    expect(page).to have_content 'ログアウトしました。'
+  end
+
+  scenario 'Not filling any field' do
+    visit root_path
+    click_link '新規登録'
+    click_button '登録'
+    expect(page).to have_content 'メールアドレスを入力してください'
+    expect(page).to have_content 'パスワードを入力してください'
+  end
+  
+  scenario 'Different password' do
+    visit root_path
+    click_link '新規登録'
+    fill_in 'shop[email]', with: 'example2@example.com'
+    fill_in 'shop[password]', with: '123'
+    fill_in 'shop[password_confirmation]', with: '456'
+    click_button '登録'
+    expect(page).to have_content 'パスワード確認とパスワードの入力が一致しません'
+    expect(page).to have_content 'パスワードは6文字以上で入力してください'
   end
 end
