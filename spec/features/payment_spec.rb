a7f8c621715f1f39e961852436de9504ad8480be
funatsu <keisuekyan2012@gmail.com>
2018-04-27 18:40:01 +0900

    Fixed link

---
+++ spec/features/payment_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.feature "Sales", type: :feature do
+    scenario 'sale new' do
+      # click 
+    end
+end
--- spec/features/sales_spec.rb
+++ spec/features/sales_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature "Sales", type: :feature do
     scenario 'visit top page' do
       # トップページを開く
       visit root_path
-      expect(page).to have_content '過去のNEM価格'
+      expect(page).to have_content '使ってみる'
     end
 end
