2a05d953cb446d95bc8474ea125f6bb636cf157b
funatsu <keisuekyan2012@gmail.com>
2018-05-01 00:43:13 +0900

    Modify api resoinse

--- app/controllers/xem_prices_controller.rb
+++ app/controllers/xem_prices_controller.rb
@@ -13,9 +13,10 @@ class XemPricesController < ApplicationController
     sale_id = params[:sale_id]
     @sale = Sale.find(sale_id)
     if @sale.confirmed == true
-      return {status: 'OK', confirmed: true, message: 'お支払が完了しました'}
+      response =  {status: 'OK', confirmed: true, message: 'お支払が完了しました'}
+    else
+      response = xem_payment(price, address, message, sale_id)
     end
-    response = xem_payment(price, address, message, sale_id)
     render json: response
   end
   def past_xem_price
